1f15a5d693d8270ae59ba4a8e338635dddba8f46
damianof <damianof@users.noreply.github.com>
2022-01-24 15:17:47 -0500

    Chapter 4 checkin

--- my-react-app/src/components/items/ItemsList.component.tsx
+++ my-react-app/src/components/items/ItemsList.component.tsx
@@ -5,23 +5,29 @@ import { ItemInterface } from '../../models/items/Item.interface'
 
 // example using class syntax
 export class ItemsListComponent extends React.Component<{
-  items: ItemInterface[]
+  items: ItemInterface[],
+  onItemSelect: (item: ItemInterface) => void
 }> {
   constructor(props: {
-    items: ItemInterface[]
+    items: ItemInterface[],
+    onItemSelect: (item: ItemInterface) => void
   }) {
     super(props)
   }
+
+  handleItemClick (item: ItemInterface) {
+    this.props.onItemSelect(item)
+  }
   
   render(): React.ReactNode {
-    const { items } = this.props
+    const { items } = this.props   
 
     return <div>
         <h3>Items:</h3>
         <ul>
           {
             items.map((item, index) => {
-              return <li key={index}>{item.name}</li>
+              return <li key={index} onClick={() => this.handleItemClick(item)}>{item.name} [{String(item.selected)}]</li>
             })
           }
         </ul>
