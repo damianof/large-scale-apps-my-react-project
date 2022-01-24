acf99957e07c846464b8e6b66bdc47f8e3d549ef
Damiano <1753185+damianof@users.noreply.github.com>
2022-01-24 15:17:47 -0500

    Chapter 4 checkin

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import './App.css'
 // import reference to our interface
 import { ItemInterface } from './models/items/Item.interface'
@@ -5,27 +7,50 @@ import { ItemInterface } from './models/items/Item.interface'
 import { ItemsListComponent } from './components/items/ItemsList.component'
 
 // mock data:
-const items: ItemInterface[] = [{
-  id: 1,
-  name: 'Item 1',
-  selected: false
-}, {
-  id: 2,
-  name: 'Item 2',
-  selected: false
-}, {
-  id: 3,
-  name: 'Item 3',
-  selected: false
-}]
+// const items: ItemInterface[] = [{
+//   id: 1,
+//   name: 'Item 1',
+//   selected: false
+// }, {
+//   id: 2,
+//   name: 'Item 2',
+//   selected: false
+// }, {
+//   id: 3,
+//   name: 'Item 3',
+//   selected: false
+// }]
 
 // component:
 function App() {
+
+  const [items, setItems] = useState<ItemInterface[]>([{
+    id: 1,
+    name: 'Item 1',
+    selected: true
+  }, {
+    id: 2,
+    name: 'Item 2',
+    selected: false
+  }, {
+    id: 3,
+    name: 'Item 3',
+    selected: false
+  }])
+
+  const onItemSelect = (item: ItemInterface) => {
+    const updatedItems = [...items]
+    const found = updatedItems.find(o => o.id === item.id) as ItemInterface
+    found.selected = !item.selected
+    setItems(updatedItems)
+    console.log('App.tsx: onItemSelect', found.id, found.selected, updatedItems)
+  }
+
   return (
     <div className="App">
-      <ItemsListComponent items={items}/>
+      <ItemsListComponent items={items} onItemSelect={onItemSelect}/>
     </div>
-  );
+  )
 }
 
 export default App
