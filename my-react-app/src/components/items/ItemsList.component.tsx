39e486a3e37ff381c9e42e123686cb1e3672222f
Damiano <1753185+damianof@users.noreply.github.com>
2022-01-26 13:43:11 -0500

    Chapter 06 - completed

--- my-react-app/src/components/items/ItemsList.component.tsx
+++ my-react-app/src/components/items/ItemsList.component.tsx
@@ -3,12 +3,12 @@
 import React from 'react'
 // import reference to our interface
 import { ItemInterface } from '../../models/items/Item.interface'
-
 // import reference to your Item component:
 import { ItemComponent } from './children/Item.component'
+// import a reference to our Loader component:
+import { Loader } from '../shared/Loader.component'
 
-
-// example using class syntax
+// ItemsList component
 export class ItemsListComponent extends React.Component<{
   loading: boolean,
   items: ItemInterface[],
@@ -28,16 +28,25 @@ export class ItemsListComponent extends React.Component<{
   
   render(): React.ReactNode {
     const { loading, items } = this.props
+    
+    let element
+    if (false) {
+      // render Loader
+      element = <Loader />
+    } else {
+      // render <ul>
+      element = <ul>
+        {
+          items.map((item, index) => {
+            return <ItemComponent key={index} model={item} onItemSelect={() => this.handleItemClick(item)}></ItemComponent>
+          })
+        }
+      </ul>
+    }
 
     return <div>
-        <h3>Items - loading: { String(loading) }:</h3>
-        <ul>
-          {
-            items.map((item, index) => {
-              return <ItemComponent key={index} model={item} onItemSelect={() => this.handleItemClick(item)}></ItemComponent>
-            })
-          }
-        </ul>
+        <h3>Items - loading: {String(loading)}:</h3>
+        {element}
       </div>
   }
 }
