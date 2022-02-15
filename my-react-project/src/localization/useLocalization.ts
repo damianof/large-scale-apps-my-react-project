e059fba15950fed9182c97ec480e634f7a640a4a
damianof <damianof@users.noreply.github.com>
2022-02-15 15:44:26 -0500

    hook naming convention (making React happy)

--- my-react-project/src/localization/useLocalization.ts
+++ my-react-project/src/localization/useLocalization.ts
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import i18n from 'i18next'
 
-export function usei18n () {
+export function useLocalization () {
   const instance = useTranslation('translation')
 
   return {
