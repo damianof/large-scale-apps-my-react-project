c5ec779057756bf6bece55d3f08b4ab6b6aad627
Damiano <1753185+damianof@users.noreply.github.com>
2022-02-24 17:41:59 -0500

    code comments

--- my-react-project/src/localization/useLocalization.ts
+++ my-react-project/src/localization/useLocalization.ts
@@ -1,24 +1,29 @@
+// file: src/localization/useLocalization.ts
+
 import { useTranslation } from 'react-i18next'
 import i18n from 'i18next'
+import { config } from '../config'
+
+// import references to our localeStorage helpers:
 import {
   getUserPreferredLocale,
   setUserPreferredLocale
  } from './i18n.init'
-import { config } from '../config'
 
+// useLocalization hook
 export function useLocalization () {
+  // we have to invoke react-i18next's useTranslation here
   const instance = useTranslation('translation')
-  const availableLocales = config.localization.locales
 
   return {
-    t: instance.t,
-    currentLocale: i18n.language,
-    changeLocale: (lcid: string) => {
+    t: instance.t, //returna the t translator function from useTranslation
+    currentLocale: i18n.language, // return the current locale from i18n
+    changeLocale: (lcid: string) => { // return helper method changeLocale
       i18n.changeLanguage(lcid)
       // also save the user preference
       setUserPreferredLocale(lcid)
     },
-    locales: availableLocales,
-    getUserPreferredLocale
+    locales: config.localization.locales, // retrun vailable locales from our config
+    getUserPreferredLocale 
   }
 }
